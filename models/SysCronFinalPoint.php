73f7076216d3b42359455487c981e7d89bb32080
uldisn <uldisnelsons@gmail.com>
2019-05-16 12:10:49 +0300

    Added sys_cron_final tabule model
    Signed-off-by: uldisn <uldisnelsons@gmail.com>

---
+++ models/SysCronFinalPoint.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace d3yii2\d3system\models;
+
+use \d3yii2\d3system\models\base\SysCronFinalPoint as BaseSysCronFinalPoint;
+
+/**
+ * This is the model class for table "sys_cron_final_point".
+ */
+class SysCronFinalPoint extends BaseSysCronFinalPoint
+{
+
+}
