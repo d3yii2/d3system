47934974e727d4840aa8be4dc893a6a0211849eb
uldisn <uldisnelsons@gmail.com>
2019-10-31 20:13:37 +0200

    added D3Module
    Signed-off-by: uldisn <uldisnelsons@gmail.com>

---
+++ yii2/base/D3Module.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace d3system\yii2\base;
+
+
+use yii\base\Module;
+
+class D3Module extends Module
+{
+    public $configFilePath;
+
+    public function __construct($id, $parent = null, $config = [])
+    {
+        if(isset($config['configFilePath'])){
+            $config = array_merge($config,include $config['configFilePath']);
+        }
+        parent::__construct($id, $parent, $config);
+    }
+
+}
