8fa68608db4e4f759a065679049e0b6550ef51b7
uldisn <uldisnelsons@gmail.com>
2019-12-10 20:33:08 +0200

    messages crud added

---
+++ messages/lv-LV/crud.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'Add' => 'Pievienot',
+    'Are you sure to delete this item?' => 'Vai tiešām dzēšat šo ierakstu?',
+    'New' => 'Jauns',
+    'Create' => 'Izveidot',
+    'Copy' => 'Izveidot kopiju',
+    'Add' => 'Pievienot',
+    'Delete' => 'Dzēst',
+    'Create new record' => 'Izveidot jaunu ierakstu',
+    'Create copy of this record' => 'Izveidot šīs kartiņas kopiju',
+    'Reset' => 'Atstatīt',
+    'Save' => 'Saglabāt',
+    'Search' => 'Meklēt',
+    'Cannot update this field' => 'Šo lauku nevar mainīt',
+    'The requested page does not exist' => 'Pieprasītā lapa neeksistē'
+
+];
