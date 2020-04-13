0586f6cf5d440e2b71a516c726f627a3060e5470
uldisn <uldisnelsons@gmail.com>
2020-04-13 12:11:13 +0300

    Final Point added methods get and save finla point
    Signed-off-by: uldisn <uldisnelsons@gmail.com>

--- models/SysCronFinalPoint.php
+++ models/SysCronFinalPoint.php
@@ -2,6 +2,7 @@
 
 namespace d3system\models;
 
+use d3system\exceptions\D3ActiveRecordException;
 use d3system\models\base\SysCronFinalPoint as BaseSysCronFinalPoint;
 
 /**
@@ -9,5 +10,29 @@ use d3system\models\base\SysCronFinalPoint as BaseSysCronFinalPoint;
  */
 class SysCronFinalPoint extends BaseSysCronFinalPoint
 {
+    public static function getFinalPointValue(string $route): int
+    {
+        return (int)self::find()
+            ->select('value')
+            ->where(['route'=>$route])
+            ->scalar();
+    }
+
+    public static function saveFinalPointValue(string $route, $value): void
+    {
+        if(!$model = self::find()
+            ->select('value')
+            ->where(['route'=>$route])
+            ->scalar()
+        ){
+            $model = new self();
+            $model->route = $route;
+        }
+        $model->value = $value;
+        if(!$model->save()){
+            throw new D3ActiveRecordException($model);
+        }
+    }
+
 
 }
