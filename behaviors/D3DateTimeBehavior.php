055df6f2533f3b2eee7935faafe0a70e4206f8ea
lauris@weberp.lv <latpages@gmail.com>
2020-04-21 17:48:51 +0300

    D3DateTimeBehavior - datumu range loģika pārnesta uz yii2-datetime DateTimeRangeAttribute klasi

--- behaviors/D3DateTimeBehavior.php
+++ behaviors/D3DateTimeBehavior.php
@@ -16,8 +16,8 @@ use yii\validators\DateValidator;
 class D3DateTimeBehavior extends DateTimeBehavior
 {
     public $originalFormat = ['date', 'yyyy-MM-dd'];
-    public $targetFormat =   ['date', 'dd.MM.yyyy'];
     public $processRange = true;
+    public $targetFormat = 'date';
 
     /**
      * @throws D3Exception
@@ -28,52 +28,11 @@ class D3DateTimeBehavior extends DateTimeBehavior
             throw new D3Exception('D3DateTimeBehavior error: Attributes are not set');
         }
 
-        parent::init();
-    }
-
-    /**
-     * @param Event $event
-     * @throws InvalidConfigException
-     */
-    public function onBeforeValidate($event): void
-    {
-        foreach ($this->attributeValues as $targetAttribute => $value) {
-            if ($value instanceof DateTimeAttribute) {
-                $validateAttr = true;
-
-                if ($this->processRange) {
-                    $originValue = $this->owner->{$value->originalAttribute . '_local'};
-                    // Check for date range, format and merge it back
-                    if ($originValue && false !== strpos($originValue, '-')) {
-                        // Split value to separate dates
-                        $dates = explode('-', $originValue);
-                        // Check both dates are given
-                        if (!empty($dates[0]) && !empty($dates[1])) {
-                            // Convert date to table format
-                            $formattedDate = $this->formatter->asDate(
-                                $dates[0],
-                                self::normalizeIcuFormat($value->originalFormat, $this->formatter)[1]
-                            );
-                            $formattedDate .= ' - ';
-                            $formattedDate .= $this->formatter->asDate(
-                                $dates[1],
-                                self::normalizeIcuFormat($value->originalFormat, $this->formatter)[1]
-                            );
-                            $this->owner->{$value->originalAttribute} = $formattedDate;
-                            $validateAttr = false;
-                        }
-                    }
-                }
-
-                if ($validateAttr) {
-                    $validator = Yii::createObject([
-                        'class' => DateValidator::class,
-                        'format' => self::normalizeIcuFormat($value->targetFormat, $this->formatter)[1],
-                    ]);
-                    $validator->validateAttribute($this->owner, $targetAttribute);
-                }
-            }
+        if ($this->processRange) {
+            $this->attributeConfig = ['class' => 'omnilight\datetime\DateTimeRangeAttribute'];
         }
+
+        parent::init();
     }
 
     /**
