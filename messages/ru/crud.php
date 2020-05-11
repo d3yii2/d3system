9eca20de48134724fb08c6f90bb16d049b7ae93d
uldisn <uldisnelsons@gmail.com>
2020-05-11 21:14:58 +0300

    Added RU translation

---
+++ messages/ru/crud.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'Add' => 'Добавить',
+    'Are you sure to delete this item?' => 'Вы уверены, что хотите удалить этот пункт?',
+    'New' => 'новый',
+    'Create' => 'Добавить',
+    'Copy' => 'Копировать',
+    'Delete' => 'Удалить',
+    'Create new record' => 'Добавить новую запись',
+    'Create copy of this record' => 'Создать копию этой записи',
+    'Reset' => 'Сбросить',
+    'Save' => 'Сохранить',
+    'Search' => 'Поиск',
+    'Cannot update this field' => 'Невозможно обновить это поле',
+    'The requested page does not exist' => 'Pieprasītā lapa neeksistē',
+    'Full' => 'Pilna',
+    'Read' => 'Skatīt',
+    'Select' => 'Выбрать'
+
+];
