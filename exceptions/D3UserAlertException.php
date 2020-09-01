18d519646305864e3716c2639bba24924c915448
uldisn <uldisnelsons@gmail.com>
2020-09-01 15:51:13 +0300

    Added D3UserAlertException()

---
+++ exceptions/D3UserAlertException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace d3system\exceptions;
+
+use yii\base\Exception;
+
+/**
+ * Class D3UserAlertException
+ * @package d3system\exceptions
+ * Use for alerting users and no loggin
+ */
+class D3UserAlertException extends Exception
+{
+
+
+}
