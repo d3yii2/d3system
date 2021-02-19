bbcbbc5f81372b2aece8ae09259e243ce39b3fe5
uldisn <uldisnelsons@gmail.com>
2021-02-19 13:54:48 +0200

    Added D3FileTarget

---
+++ yii2/log/D3FileTarget.php
@@ -0,0 +1,88 @@
+<?php
+
+
+namespace d3system\yii2\log;
+
+
+use yii\log\FileTarget;
+use yii\log\Logger;
+use yii\web\Request;
+use yii;
+
+class D3FileTarget extends FileTarget
+{
+    public $showMessageInfo = [
+        'time',
+        'ip',
+        'user',
+        'sysCompany',
+        'level',
+        'category',
+        'text',
+        'trace'
+    ];
+    public function formatMessage($message): string
+    {
+        [$text, $level, $category, $timestamp] = $message;
+
+        $rowText = '';
+
+        if($this->isShowInfo('time')) {
+            $rowText .= $this->getTime($timestamp);
+        }
+
+        if(yii::$app !== null) {
+            if ($this->isShowInfo('ip')) {
+                $request = Yii::$app->getRequest();
+                $ip = $request instanceof Request ? $request->getUserIP() : '-';
+                $rowText .= '[' . $ip . ']';
+            }
+
+            if ($this->isShowInfo('user')) {
+                /* @var $user \yii\web\User */
+                $user = yii::$app->has('user', true) ? Yii::$app->get('user') : null;
+                if ($user && ($identity = $user->getIdentity(false))) {
+                    $userID = $identity->username;
+                } else {
+                    $userID = '-';
+                }
+                $rowText .= '[' . $userID . ']';
+            }
+
+            if ($this->isShowInfo('sysCompany')) {
+                /* @var $user \yii\web\User */
+                if($sysCompany = Yii::$app->has('SysCmp') ? Yii::$app->get('SysCmp') : null){
+                    $rowText .= '[' . $sysCompany->getActiveCompanyName() . ']';
+                }else{
+                    $rowText .= '[-]';
+                }
+            }
+        }
+
+        if($this->isShowInfo('level')){
+            $rowText .= '[' . Logger::getLevelName($level) . ']';
+        }
+        if($this->isShowInfo('category')){
+            $rowText .= '[' . $category . ']';
+        }
+        if($this->isShowInfo('text')){
+            $rowText .= ' ' . $text;
+        }
+        if($this->isShowInfo('trace') && isset($message[4])){
+            $traces = [];
+            foreach ($message[4] as $trace) {
+                $traces[] = "in {$trace['file']}:{$trace['line']}";
+            }
+            if(!empty($trace)) {
+                $rowText .= "\n    " . implode("\n    ", $traces);
+            }
+        }
+        return $rowText;
+    }
+
+    public function isShowInfo(string $info)
+    {
+        return in_array($info,$this->showMessageInfo);
+    }
+
+}
