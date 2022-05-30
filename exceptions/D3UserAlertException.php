ab5ddf4b6c46caf9ce641beca6461d910cf88b25
uldisn <uldisnelsons@gmail.com>
2022-05-30 10:13:40 +0300

    D3UserAlertException deprecated
    Signed-off-by: uldisn <uldisnelsons@gmail.com>

--- exceptions/D3UserAlertException.php
+++ exceptions/D3UserAlertException.php
@@ -8,6 +8,7 @@ use yii\base\Exception;
  * Class D3UserAlertException
  * @package d3system\exceptions
  * Use for alerting users and no loggin
+ * @deprecated  please use yii\base\UserException
  */
 class D3UserAlertException extends Exception
 {
