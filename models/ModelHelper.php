bd4b816bdeda69455ba1b3b1ac9417c196929666
uldis <uldis@weberp.lv>
2025-02-19 11:49:32 +0200

    Add ModelHelper class for normalizing float attributes
    Introduce a helper class to standardize float data attributes in models. The class ensures attributes are set to 0 or converted into valid floats, enhancing data integrity during model loading.

---
+++ models/ModelHelper.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace d3system\models;
+
+class ModelHelper
+{
+    /**
+     * A helper method to normalize float attributes.
+     * Ensures attributes are either 0 or valid floats.
+     *
+     * implementing:
+     * ------------------------------------------------------------------------------
+     * private const FLOAT_ATTRIBUTES = ['fee','fee_amount','recipient_fee','recipient_fee_amount'];
+     *
+     * public function rules(): ?array
+     * {
+     *      return array_merge(
+     *          parent::rules(),
+     *          [
+     *              [self::FLOAT_ATTRIBUTES,'number']
+     *          ]
+     *      );
+     * }
+     *
+     * public function load($data, $formName = null): bool
+     * {
+     *      $scope = $formName ?? $this->formName();
+     *      $attributes = &$data[$scope];
+     *      ModelHelper::normalizeFloatDataAttributes(self::FLOAT_ATTRIBUTES, $attributes);
+     *      return parent::load($data, $formName);
+     * }
+     *
+     * @param array $floatAttributes
+     * @param array $attributes
+     */
+    public static function normalizeFloatDataAttributes(array $floatAttributes, array &$attributes): void
+    {
+        foreach ($floatAttributes as $attributeName) {
+            if (!isset($attributes[$attributeName])) {
+                $attributes[$attributeName] = 0;
+            } else {
+                $attributes[$attributeName] = is_numeric($attributes[$attributeName])
+                    ? (float)$attributes[$attributeName]
+                    : $attributes[$attributeName];
+            }
+        }
+    }
+}
