aaf07a9912f661fef892e67e7614ebed4edc8169
uldis <uldis@weberp.lv>
2026-03-14 19:37:05 +0200

    Deprecate `D3ActiveRecordException`, introduce `D3ActiveRecordException2` with enhanced debug capabilities and updated structure

--- exceptions/D3ActiveRecordException2.php
+++ exceptions/D3ActiveRecordException2.php
@@ -7,29 +7,19 @@ use yii\db\ActiveRecord;
 use yii\helpers\VarDumper;
 
 /**
- * Exception for catching active record errors
- * Class D3ActiveRecordException
- * @package d3system\exceptions
- * @deprecated use d3system\exceptions\D3ActiveRecordException2
+ * Exception class for handling errors related to ActiveRecord model operations.
+ * Added extra data for debug and Sentry.
  */
-class D3ActiveRecordException extends Exception
+class D3ActiveRecordException2 extends Exception
 {
     public object $model;
 
     /**
      * D3ModelException constructor.
      * @param ActiveRecord|Object $model
-     * @param string|null $flashMessage message for displaying in flash
-     * @param bool|false $loggingMessage logging message. If false, do not log
-     * @param array|bool $flashAttributes list attributes for displaying in flash. If false, do not show. If true, show all
-     * @param string $errorCategory
      */
     public function __construct(
-        $model,
-        string $flashMessage = null,
-        bool $loggingMessage = true,
-        $flashAttributes = false,
-        string $errorCategory = 'application'
+        object $model
     ) {
         $this->model = $model;
         $modelErrors = 'Can\'t save ' . get_class($model);
